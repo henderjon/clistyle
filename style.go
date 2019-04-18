476b10ec00d8693377ab49c8fcf5d3bc12ac45ba
henderjon <list@box370.net>
2019-04-18 12:43:02 -0500

    you have to start somewhere and I think I'll start here

---
+++ style.go
@@ -0,0 +1,123 @@
+package clistyle
+
+import (
+	"strings"
+)
+
+// These are the bits to use in setting the flag
+const (
+	esc       = "\u001b[" // "\033", byte(27)
+	reset     = ";0m"
+	bold      = ";1"
+	underline = ";4"
+	reverse   = ";7"
+	black     = ";30"
+	red       = ";31"
+	green     = ";32"
+	yellow    = ";33"
+	blue      = ";34"
+	magenta   = ";35"
+	cyan      = ";36"
+	white     = ";37"
+	blackBG   = ";40"
+	redBG     = ";41"
+	greenBG   = ";42"
+	yellowBG  = ";43"
+	blueBG    = ";44"
+	magentaBG = ";45"
+	cyanBG    = ";46"
+	whiteBG   = ";47"
+	Bold      = 1 << iota
+	Underline
+	Reverse
+	Bright
+	Black
+	Red
+	Green
+	Yellow
+	Blue
+	Magenta
+	Cyan
+	White
+	BlackBG
+	RedBG
+	GreenBG
+	YellowBG
+	BlueBG
+	MagentaBG
+	CyanBG
+	WhiteBG
+)
+
+var texts = map[int]string{
+	Reverse:   reverse,
+	Underline: underline,
+	Bold:      bold,
+}
+var foregrounds = map[int]string{
+	Black:   black,
+	Red:     red,
+	Green:   green,
+	Yellow:  yellow,
+	Blue:    blue,
+	Magenta: magenta,
+	Cyan:    cyan,
+	White:   white,
+}
+var backgrounds = map[int]string{
+	BlackBG:   blackBG,
+	RedBG:     redBG,
+	GreenBG:   greenBG,
+	YellowBG:  yellowBG,
+	BlueBG:    blueBG,
+	MagentaBG: magentaBG,
+	CyanBG:    cyanBG,
+	WhiteBG:   whiteBG,
+}
+
+// Style decorates value according to the options passed via flag. It only allows three settings
+// [text;foreground;backgroundm
+func Style(value string, flag int) string {
+	var (
+		s strings.Builder
+		tx,
+		fg,
+		bg string
+	)
+	for b, s := range texts {
+		if has(flag, b) {
+			tx = s
+		}
+	}
+	for b, s := range foregrounds {
+		if has(flag, b) {
+			fg = s
+		}
+	}
+	for b, s := range backgrounds {
+		if has(flag, b) {
+			bg = s
+		}
+	}
+
+	if len(tx)+len(fg)+len(bg) > 0 {
+		s.WriteString(esc)
+		s.WriteString(tx)
+		s.WriteString(fg)
+		s.WriteString(bg)
+		s.WriteString("m") // ? ... so strange
+		s.WriteString(value)
+		s.WriteString(esc)
+		s.WriteString(reset)
+	} else {
+		return value
+	}
+
+	return s.String()
+}
+
+// https://yourbasic.org/golang/bitmask-flag-set-clear/
+// func set(b, flag int) int    { return b | flag }
+// func clear(b, flag int) int  { return b &^ flag }
+// func toggle(b, flag int) int { return b ^ flag }
+func has(b, flag int) bool { return b&flag != 0 }
---
+++ style_test.go
@@ -0,0 +1,27 @@
+package clistyle
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestString(t *testing.T) {
+	e := Style("test this string", 0)
+
+	expected := "test this string"
+	if diff := cmp.Diff(e, expected); diff != "" {
+		t.Error("String(); (-got +want)", diff)
+	}
+}
+
+func TestStyle(t *testing.T) {
+	e := Style("test this string", Bold|YellowBG)
+
+	// log.Fatalf("%#v", e)
+	// `\x1b[;1;43mtest this string\x1b[;0m`
+	expected := "\x1b[;1;43mtest this string\x1b[;0m"
+	if diff := cmp.Diff(e, expected); diff != "" {
+		t.Error("Style(); (-got +want)", diff)
+	}
+}
